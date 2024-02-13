31e4f9d94a8409abfbb3c4dba764acffe0e03e0a
VIVEK SINGH POLYWAL <74926635+viveksinghpolywal@users.noreply.github.com>
2024-02-14 01:08:42 +0530

    Time: 12 ms (19.39%), Space: 71.3 MB (6.04%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -6,51 +6,27 @@
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public boolean isPalindrome(ListNode head) 
-    {    
-        ListNode mid = middleNode(head);
-        ListNode s = reverse(mid);
-        ListNode f = head;
-        while( head != null && s != null)
-        {
-            if(f.val != s.val)
-            return false; 
+ **/
+ 
+ public class Solution {
 
-             f = f.next;
-             s = s.next;
+    ListNode frontPointer;
+
+    public boolean isPalindrome(ListNode head) {
+        frontPointer = head;
+        return isPalindrome2PointO(head);
+    }
+
+    private boolean isPalindrome2PointO(ListNode currentNode) {
+        if (currentNode != null) {
+            if (!isPalindrome2PointO(currentNode.next)) {
+                return false;
+            }
+            if (currentNode.val != frontPointer.val) {
+                return false;
+            }
+            frontPointer = frontPointer.next;
         }
         return true;
-    }
-
-    public ListNode middleNode(ListNode head)
-    {
-        ListNode fast = head;
-        ListNode slow = head;
-        while(fast != null && fast.next != null)
-        {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
-    public ListNode reverse(ListNode mid)
-    {    
-        if(mid == null) return mid;
-        ListNode prev = null;
-        ListNode present = mid;
-        ListNode then = present.next;
-        while(present != null)
-        {   
-           present.next = prev;
-           prev = present;
-           present = then;
-           if(then != null)
-           then = then.next;
-    
-        }
-        return prev;
     }
 }
