99768849d63999dcf70a2e6dc727dcc83582c4db
VIVEK SINGH POLYWAL <74926635+viveksinghpolywal@users.noreply.github.com>
2024-02-28 02:23:32 +0530

    Time: 10 ms (92.99%), Space: 45.7 MB (80.6%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public Solution(ListNode head) {
+        while(head!=null){
+            list.add(head.val);
+            head=head.next;
+        }
+    }
+    
+    public int getRandom() {
+        double rand=Math.random()*list.size();
+        return list.get((int)rand);
+    }
+}
