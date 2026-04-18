0078b72ccdc7ea36154858ad1641329e43b8ac0d
VIVEK SINGH POLYWAL <74926635+viveksinghpolywal@users.noreply.github.com>
2026-04-18 21:36:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,16 +3,16 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let insertPos = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            nums[insertPos] = nums[i];
-            insertPos++;
+    let swapindex=0;
+    for(let i=0;i<nums.length;i++){
+        
+        if(nums[i]!==0){
+            nums[swapindex]=nums[i];
+            swapindex++;
         }
+
     }
-    while (insertPos < nums.length) {
-        nums[insertPos] = 0;
-        insertPos++;
+    for(let x=swapindex; x<nums.length;x++){
+        nums[x]=0;
     }
 };
