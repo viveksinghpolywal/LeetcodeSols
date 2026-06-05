78629a878f93c7b4ddae8bf5773df837ff9ddade
VIVEK SINGH POLYWAL <74926635+viveksinghpolywal@users.noreply.github.com>
2026-06-06 01:49:12 +0530

    Time: 0 ms (100%), Space: 43.8 MB (63.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,1 +1,33 @@
-class Solution { public int search(int[] nums, int target) { for (int i = 0; i < nums.length; i++) { if (nums[i] == target) { return i; } } return -1; } }
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int n=nums.length;
+        int high=n-1;
+        int output=-1;
+        while(low<=high){
+            // if(low==high && nums[low]==target){
+            //     output=low;
+            //     break;    
+            // }
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) output=mid;
+            if(nums[low]<=nums[mid] ){
+                if(target>=nums[low] && target<=nums[mid]){
+                    high=mid-1;
+                }
+               else{ low=mid+1;}
+            }
+            else {
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{high=mid-1;}
+                
+                
+            }
+            
+
+        }
+        return output;
+    }
+}
