215e6d359ae96d8b7ba89f213e1f1b6db64da26e
VIVEK SINGH POLYWAL <74926635+viveksinghpolywal@users.noreply.github.com>
2026-06-20 01:24:42 +0530

    Time: 0 ms (100%), Space: 44 MB (23.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,17 +1,3 @@
-// class Solution {
-//     public boolean searchMatrix(int[][] matrix, int target) {
-//        int n=matrix.length;
-//        int m=matrix[0].length;
-//        int low=0;
-//        int high=n-1;
-//        int low2=0;
-//        int high2=m-1;
-//        while(low<=high){
-//         int mid=(low+high)/2;
-//         if
-//        } 
-//     }
-// }
 
 class Solution {
 
