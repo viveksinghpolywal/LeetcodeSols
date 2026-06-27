b77bc31c7b1bea8a522e0d0078d1e84e9adb10e1
VIVEK SINGH POLYWAL <74926635+viveksinghpolywal@users.noreply.github.com>
2026-06-28 03:03:25 +0530

    Time: 12 ms (86.61%), Space: 47.5 MB (19.29%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        List<Character>[] bucket=new ArrayList[s.length()+1];
+
+        for(char ch: map.keySet()){
+            int freq=map.get(ch);
+            if(bucket[freq]==null){
+                bucket[freq]=new ArrayList<>();
+            }
+            bucket[freq].add(ch);
+        }
+
+        StringBuilder ans=new StringBuilder();
+        for(int i=bucket.length-1;i>=0;i--){
+            if(bucket[i] == null)
+                continue;
+
+             for(char ch : bucket[i]) {
+
+                for(int j = 0; j < i; j++) {
+                    ans.append(ch);
+                }
+            }
+        }
+        return ans.toString();
+    }
+}
