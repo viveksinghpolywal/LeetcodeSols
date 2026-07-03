1c224cf22c91905e346ad438f765f72f02f4ea82
VIVEK SINGH POLYWAL <74926635+viveksinghpolywal@users.noreply.github.com>
2026-07-03 23:18:08 +0530

    Time: 1 ms (100%), Space: 44 MB (25.08%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,39 +1,37 @@
 class Solution {
     public int myAtoi(String s) {
-        int n = s.length();
-        int i = 0;
-        int sign = 1;
-        while(i<n && s.charAt(i)==' ') {
-            i++;
-        }
-        if (i>=n) {
-            return 0;
-        }
-        if (s.charAt(i)=='-') {
-            sign = -1;
-        }
-        if (s.charAt(i)=='+' || s.charAt(i)=='-') {
+        int n=s.length();
+        int sign=1;
+         int i = 0;
+        while(i<n && s.charAt(i)==' '){
             i++;
         }
 
-        if (i>=n) {
+        if (i == n) {
             return 0;
-        }
-        int num = 0;
-        while (i<n && s.charAt(i)>='0' && s.charAt(i)<='9') {
-            int x = s.charAt(i)-'0';
-            //System.out.println(x);
-            if (num>Integer.MAX_VALUE/10 || (num==Integer.MAX_VALUE/10 && x>Integer.MAX_VALUE%10)) {
-                if (sign==1) {
-                    return Integer.MAX_VALUE;
-                }
-                else {
-                    return Integer.MIN_VALUE;
-                }
+        } 
+
+        if(s.charAt(i)=='-' || s.charAt(i)=='+'){
+            if(s.charAt(i)=='-'){
+                sign=-1;
             }
-            num = num*10 + x;
             i++;
         }
-        return num*sign;
+
+        int ans = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            if (ans > Integer.MAX_VALUE / 10 ||
+               (ans == Integer.MAX_VALUE / 10 && digit > 7)) {
+
+                return sign == 1
+                        ? Integer.MAX_VALUE
+                        : Integer.MIN_VALUE;
+            }
+             ans = ans * 10 + digit;
+             i++;
+        }
+        return ans * sign;
     }
 }
